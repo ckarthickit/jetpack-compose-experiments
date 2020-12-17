89e18baa51a4e5408e90fdd8d815077b501d25ca
Karthick Chinnathambi <ckarthickit@gmail.com>
2020-12-18 04:13:04 +0530

    Add loadImageResource, Update README

--- app/src/main/java/com/kartdroid/jetpackcomposeexp/MainActivity2.kt
+++ app/src/main/java/com/kartdroid/jetpackcomposeexp/MainActivity2.kt
@@ -2,6 +2,7 @@ package com.kartdroid.jetpackcomposeexp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.Text
 import androidx.compose.foundation.layout.Column
@@ -14,10 +15,10 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.imageFromResource
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.res.loadImageResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
@@ -28,7 +29,10 @@ class MainActivity2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MaterialThemedSurface {
-                NewStory()
+                Column() {
+                    NewStory()
+                    AnotherStory(R.drawable.header)
+                }
             }
         }
     }
@@ -68,10 +72,49 @@ fun NewStory() {
 }
 
 
+@Composable
+fun AnotherStory(@DrawableRes resId: Int) {
+    val dynamicImage = loadImageResource(id = resId)
+    dynamicImage.resource.resource?.let {
+        Column(modifier = Modifier.padding(8.dp)) {
+            val imageModifier = Modifier
+                .preferredHeight(240.dp)
+                .fillMaxWidth()
+                .padding(8.dp)
+                .clip(RoundedCornerShape(8.dp))
+            Image(
+                asset = it,
+                modifier = imageModifier,
+                contentScale = ContentScale.Crop
+            )
+            Spacer(modifier = Modifier.preferredHeight(16.dp))
+            Text(
+                "A day wandering through the sandhills in Shark Fin Cove," +
+                        " and a few of the sights I saw",
+                style = MaterialTheme.typography.h6,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis
+            )
+            Text(
+                "Davenport, California",
+                style = MaterialTheme.typography.body2
+            )
+            Text(
+                "December 2020",
+                style = MaterialTheme.typography.body2
+            )
+        }
+    }
+}
+
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultM2Preview() {
     MaterialThemedSurface {
-        NewStory()
+        Column() {
+            NewStory()
+            AnotherStory(R.drawable.header)
+        }
     }
 }
