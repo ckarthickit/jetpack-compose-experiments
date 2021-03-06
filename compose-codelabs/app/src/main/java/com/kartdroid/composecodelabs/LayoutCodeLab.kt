ee50e388224bbedc2b29823c70f3bb254b8dacf8
Karthick Chinnathambi <ckarthickit@gmail.com>
2021-03-06 22:38:56 +0530

    Add Layout Codelab

---
+++ compose-codelabs/app/src/main/java/com/kartdroid/composecodelabs/LayoutCodeLab.kt
@@ -0,0 +1,87 @@
+package com.kartdroid.composecodelabs
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ContentAlpha
+import androidx.compose.material.LocalContentAlpha
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.font.FontWeight.Companion
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.kartdroid.composecodelabs.ui.theme.ComposeCodeLabsTheme
+
+class LayoutCodeLab : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LayoutComposeDemo()
+        }
+    }
+}
+
+@Composable
+fun LayoutComposeDemo() {
+    ComposeCodeLabsTheme {
+        Surface {
+            PhotographerCard()
+        }
+    }
+}
+
+/************* PHOTOGRAPHER CARD ************/
+
+@Composable
+fun PhotographerCard() {
+    Row(
+        modifier = Modifier
+            .padding(4.dp)
+            .clip(RoundedCornerShape(4.dp))
+            .background(MaterialTheme.colors.surface)
+            .clickable {  }
+            .padding(8.dp)
+    ) {
+        Surface(
+            modifier = Modifier.size(50.dp),
+            shape = CircleShape,
+            color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
+        ) {
+
+        }
+        Column(
+            modifier = Modifier
+                .padding(4.dp)
+                .align(Alignment.CenterVertically) //Available only in RowScope
+        ) {
+            Text(text = "Alfred Sisley", fontWeight = FontWeight.Bold)
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                Text(text = "3 minutes ago", style = MaterialTheme.typography.body2)
+            }
+            //Text(text = "3 minutes ago", style = MaterialTheme.typography.body2)
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun Preview() {
+    LayoutComposeDemo()
+}
