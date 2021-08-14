f5902968222e6ea257f78aa490337f496fe32dc5
Karthick Chinnathambi <karthick.chinnathambi@byjus.com>
2021-08-14 21:19:51 +0530

    [StateCodelab] Improve Todo Experiment

--- compose-codelabs/app/src/main/java/com/kartdroid/composecodelabs/statecodelab/todo/model/TodoItem.kt
+++ compose-codelabs/app/src/main/java/com/kartdroid/composecodelabs/statecodelab/todo/model/TodoItem.kt
@@ -5,6 +5,9 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material.icons.filled.Call
 import androidx.compose.material.icons.filled.Done
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.ui.graphics.vector.ImageVector
 import com.kartdroid.composecodelabs.R.string
 import java.util.UUID
@@ -17,9 +20,12 @@ data class TodoItem(
 )
 
 enum class TodoIcon(val icon: ImageVector, @StringRes val contentDescription: Int){
-    Square(Icons.Default.AccountBox, string.icon_desc_account),
     Done(Icons.Default.Done, string.icon_desc_done),
-    Call(Icons.Default.Call , string.icon_desc_done)
+    Square(Icons.Default.AccountBox, string.icon_desc_account),
+    Call(Icons.Default.Call , string.icon_desc_done),
+    Settings(Icons.Default.Settings, string.icon_desc_settings),
+    Event(Icons.Default.Email, string.icon_desc_event),
+    Account(Icons.Default.Star, string.icon_desc_star)
 }
 
 
