b37f463ee73a845b849a91f0125ece2d6ecac569
Karthick Chinnathambi <karthick.chinnathambi@byjus.com>
2021-09-26 20:43:24 +0530

    MealDB Added Hilt , Spotless and Detekt Dependencies

--- meal-db/buildSrc/src/main/kotlin/ProjectDependencies.kt
+++ meal-db/buildSrc/src/main/kotlin/ProjectDependencies.kt
@@ -4,20 +4,35 @@ object Versions {
 
     const val coroutinesVersion = "1.5.1"
     const val composeVersion = "1.0.0"
-    const val timberLibraryVersion = "4.7.1"
     const val retrofitVersion = "2.9.0"
     const val okHttpVersion = "4.3.1"
     const val moshiVersion = "1.12.0"
+
+    const val androidXLifecycle = "2.2.0"
+
+    const val hilt = "2.38.1"
+    const val hiltLifecycle = "1.0.0"
+
+    const val timberLibraryVersion = "4.7.1"
+
+    const val DetektVersion = "1.18.1"
+    const val SpotlessVersion = "5.15.1"
 }
 
 enum class BuildScriptPlugins(val artifact: String) {
     ANDROID_BUILD_TOOLS("com.android.tools.build:gradle:${Versions.androidBuildToolsPlugin}"),
     KOTLIN("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlinVersion}"),
+    HILT("com.google.dagger:hilt-android-gradle-plugin:${Versions.hilt}"),
 }
 
 object Deps {
     object Google {
         const val Material = "com.google.android.material:material:1.4.0"
+
+        object Hilt {
+            const val Core = "com.google.dagger:hilt-android:${Versions.hilt}"
+            const val Compiler = "com.google.dagger:hilt-android-compiler:${Versions.hilt}"
+        }
     }
 
     object Jetbrains {
@@ -53,6 +68,12 @@ object Deps {
         const val CoreKTX = "androidx.core:core-ktx:1.6.0"
         const val AppCompat = "androidx.appcompat:appcompat:1.3.1"
 
+        object Hilt {
+            //Hilt Extensions
+            const val Lifecycle = "androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha03"
+            const val Compiler = "androidx.hilt:hilt-compiler:${Versions.hiltLifecycle}"
+        }
+
         object Compose {
             const val Foundation = "androidx.compose.foundation:foundation:${Versions.composeVersion}"
             const val UITooling = "androidx.compose.ui:ui-tooling:${Versions.composeVersion}"
@@ -69,6 +90,12 @@ object Deps {
         object Activity {
             const val Compose = "androidx.activity:activity-compose:1.3.0"
         }
+
+        object Lifecycle {
+            val viewModelKTX = "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.androidXLifecycle}"
+            val liveDataKTX = "androidx.lifecycle:lifecycle-livedata-ktx:${Versions.androidXLifecycle}"
+        }
+        const val fragmentKTX = "androidx.fragment:fragment-ktx:1.3.5"
 
         object Test {
             const val JunitExt = "androidx.test.ext:junit:1.1.3"
