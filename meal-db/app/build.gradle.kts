38ea92c59722b047cf44ceeedd4ba66386d6eef0
Karthick Chinnathambi <karthick.chinnathambi@byjus.com>
2021-09-26 20:07:08 +0530

    MealdB Add Network related dependencies

--- meal-db/app/build.gradle.kts
+++ meal-db/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
+    id("kotlin-kapt")
 }
 
 android {
@@ -18,7 +19,7 @@ android {
 
     buildTypes {
         getByName("release") {
-            this.isMinifyEnabled =  false
+            this.isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
@@ -36,11 +37,13 @@ android {
 }
 
 dependencies {
+
     implementation(Deps.AndroidX.CoreKTX)
     implementation(Deps.AndroidX.AppCompat)
 
     implementation(Deps.Google.Material)
 
+    //Use Jetpack Compose
     implementation(Deps.AndroidX.Compose.Foundation)
     implementation(Deps.AndroidX.Compose.UITooling)
     implementation(Deps.AndroidX.Compose.UI)
@@ -48,6 +51,32 @@ dependencies {
     implementation(Deps.AndroidX.Compose.Runtime)
     implementation(Deps.AndroidX.Compose.RuntimeLiveData)
 
+    // Use Kotlin Coroutines
+    implementation(Deps.Jetbrains.Coroutines)
+
+    //Use Retrofit for NW Calls
+    implementation(Deps.Square.Retrofit.Core)
+    //Retrofit Converters
+    implementation(Deps.Square.Retrofit.ConverterScalars)
+    implementation(Deps.Square.Retrofit.ConverterMoshi)
+    //implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:0.8.0")
+    implementation(Deps.Square.Retrofit.Mock)
+
+    //OkHttp and it's interceptors
+    implementation(Deps.Square.OkHttp.Core)
+    implementation(Deps.Square.OkHttp.LoggingInterceptor)
+    implementation(Deps.Square.OkHttp.MockWebServer)
+
+    //Use Moshi for Serialization / De-Serialization
+
+    implementation(Deps.Square.Moshi.Core)
+    implementation(Deps.Square.Moshi.Adapters)
+    kapt(Deps.Square.Moshi.KotlinCodeGen)
+
+    // Use the Kotlin test library.
+    testImplementation(Deps.Jetbrains.KotlinTest)
+    // Use the Kotlin JUnit integration.
+    testImplementation(Deps.Jetbrains.KotlinTestJunit)
     testImplementation(Deps.Junit.Junit)
     androidTestImplementation(Deps.AndroidX.Test.JunitExt)
     androidTestImplementation(Deps.AndroidX.Test.EspressoCore)
